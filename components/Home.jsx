2826d2525270b74103306b5830d7fb3024e6f4c5
izzatillonemataliev <156827918+izzatillonemataliev@users.noreply.github.com>
2024-06-11 21:56:04 +0500

    wefofof

--- components/Home.jsx
+++ components/Home.jsx
@@ -35,15 +35,7 @@ function Home() {
           width={900}
         />
         <p style={{ textAlign: "center" }}>
-          Naruto[a] is a Japanese manga series written and illustrated by
-          Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja
-          who seeks recognition from his peers and dreams of becoming the
-          Hokage, the leader of his village. The story is told in two parts: the
-          first is set in Naruto's pre-teen years (volumes 1–27), and the second
-          in his teens (volumes 28–72). The series is based on two one-shot
-          manga by Kishimoto: Karakuri (1995), which earned Kishimoto an
-          honorable mention in Shueisha's monthly Hop Step Award the following
-          year, and Naruto (1997).
+          
         </p>
 
         <Link
