217c3569affed82140cf7bb580b02c4c0f25bf77
izzatillonemataliev <156827918+izzatillonemataliev@users.noreply.github.com>
2024-06-11 22:10:52 +0500

    do[fo[f

--- components/About.jsx
+++ components/About.jsx
@@ -3,13 +3,14 @@ function About() {
     <div>
       <h2 style={{ textAlign: "center" }}>About</h2>
       <img
-        style={{ borderRadius: "10px" }}
+        style={{ borderRadius: "0" }}
         src="https://mi-store.uz/wp-content/uploads/2024/03/redmi20note201320global.png"
         alt=""
-        width={500}
-        height={300}
+        width={300}
+        height={500}
       />
       <p style={{ textAlign: "center", fontSize: "24px" }}></p>
+      Сегодня Xiaomi представила серию Redmi Note 13 на презентации в Таиланде, Бангкок. Эта невероятная серия включает в себя пять устройств, которые еще больше расширяют популярную линейку Redmi Note: Redmi Note 13 Pro+ 5G, Redmi Note 13 Pro и Redmi Note 13. 
     </div>
   );
 }
